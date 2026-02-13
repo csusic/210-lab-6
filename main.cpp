ce32d20245f8adf6edb2df1ea55eb65c9090b738
Christine Susic <csusic022@insite.4cd.edu>
2026-02-12 17:34:10 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -14,17 +14,36 @@ const int SIZE = 5;
 int main() {
     //create pointer that points to new array
     double *ptr = nullptr;
+    //array of doubles
     ptr = new double [SIZE]; //new array
     
+    enterArrayData();
+    outputArrayData();
+    sumArray();
+    
+    return 0;
+}
+
+//receives dynamic double array 
+//populates it with user values
+double enterArrayData(double []) {
     //populate array
     for (int i = 0; i < SIZE; i++)
         ptr[i];
-        
+    return;
+}
+
+//receives dynamic double array
+//outputs contents on one line
+double outputArrayData(double []) {
     //output array
     for (int i = 0; i < SIZE; i++)
         cout << ptr[i] << " ";
-        
-        
+    return;
+}
+
+//receives dynamic double array
+//calculates and returns sum
+void sumArray(double []) {
     
-    return 0;
 }
