2794170da38e6ab2be12a609cd7a9d142e260b00
Christine Susic <csusic022@insite.4cd.edu>
2026-02-12 17:16:03 -0800

    Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// COMSC-210 | Lab 6 | Christine Susic
+
+#include <iostream>
+using namespace std;
+
+//function prototypes
+double enterArrayData(double []);
+double outputArrayData(double []);
+void sumArray(double []);
+
+int main() {
+    
+    return 0;
+}
