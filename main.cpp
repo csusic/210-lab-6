d1a989ba79348fd6f05d9c96c3b8889ba32a267a
Christine Susic <csusic022@insite.4cd.edu>
2026-02-12 17:24:11 -0800

    Commit

--- main.cpp
+++ main.cpp
@@ -8,7 +8,23 @@ double enterArrayData(double []);
 double outputArrayData(double []);
 void sumArray(double []);
 
+//size of array
+const int SIZE = 5;
+
 int main() {
+    //create pointer that points to new array
+    double *ptr = nullptr;
+    ptr = new double [SIZE]; //new array
+    
+    //populate array
+    for (int i = 0; i < SIZE; i++)
+        ptr[i];
+        
+    //output array
+    for (int i = 0; i < SIZE; i++)
+        cout << ptr[i] << " ";
+        
+        
     
     return 0;
 }
